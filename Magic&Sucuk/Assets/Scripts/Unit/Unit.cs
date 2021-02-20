800d5fb3aa1aa2f825840bfc4663a265e8afae94
MelihCelik00 <melihsafa.c@gmail.com>
2021-02-20 19:01:46 +0300

    staged scripts for backup

--- Magic&Sucuk/Assets/Scripts/Unit/Unit.cs
+++ Magic&Sucuk/Assets/Scripts/Unit/Unit.cs
@@ -8,6 +8,12 @@ namespace Unit
         public int unitLevel;
         
         public int damage;
+        public int magicDamage;
+        
+        public int strikeCoefficient;
+        public int waterCoefficient;
+        public int streamCoefficient;
+        public int fireCoefficient;
         
         public int maxHP;
         public int currentHP;
@@ -29,6 +35,22 @@ namespace Unit
             {
                 currentHP = maxHP;
             }
+        }
+
+        public void SetUnit(string name, int dmg, int magicDmg, int maxHp, int currHp,int strikec, int waterc, int streamc, int firec)
+        {
+            unitName = name;
+            damage = dmg;
+            magicDamage = magicDmg;
+        
+            strikeCoefficient = strikec;
+            waterCoefficient = waterc;
+            streamCoefficient = streamc;
+            fireCoefficient = firec;
+        
+            maxHP = maxHp;
+            currentHP = currHp;
+            
         }
         
         
