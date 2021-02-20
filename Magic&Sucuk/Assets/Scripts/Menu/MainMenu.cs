3a700780f26c577d5317670d6d5532b5960eae9b
MelihCelik00 <melihsafa.c@gmail.com>
2021-02-20 03:23:29 +0300

    main menu is done, scripts class is created

---
+++ Magic&Sucuk/Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Menu
+{
+    public class MainMenu : MonoBehaviour
+    {
+        public GameObject mainMenu;
+        public GameObject htpMenu;
+        public GameObject creditsMenu;
+
+        public void PlayGame()
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+        }
+
+        public void HowToPlayMenu()
+        {
+            mainMenu.SetActive(false);
+            htpMenu.SetActive(true);
+        }
+
+        public void Credits()
+        {
+            mainMenu.SetActive(false);
+            creditsMenu.SetActive(true);
+        }
+        
+        public void QuitGame()
+        {
+            Application.Quit();
+        }
+
+        public void GoBack()
+        {
+            if (htpMenu.activeSelf)
+            {
+                htpMenu.SetActive(false);
+            }else if (creditsMenu.activeSelf)
+            {
+                creditsMenu.SetActive(false);
+            }
+            mainMenu.SetActive(true);
+            
+        }
+    }
+}
