ede2e8bf8c6032114fef69ca9259bfc91883ac36
MelihCelik00 <melihsafa.c@gmail.com>
2021-02-20 19:57:00 +0300

    updated boss class, added tag, modified scene

---
+++ Magic&Sucuk/Assets/Scripts/Unit/BossClass.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using Managers;
+
+namespace Unit
+{
+    public class BossClass : MonoBehaviour
+    {
+        public Unit unit;
+        public BattleHUD enemyHUD;
+        private void Start()
+        {
+            unit = GameObject.FindGameObjectWithTag("pinkcloyd").GetComponent<Unit>();
+            unit.SetUnit("Pink Cloyd",20,20, 600, 600,1,0,1,1);
+            enemyHUD = GameObject.FindGameObjectWithTag("enemyHUD").GetComponent<BattleHUD>();
+            enemyHUD.SetHUD(unit);                  
+        }
+    }
+}
