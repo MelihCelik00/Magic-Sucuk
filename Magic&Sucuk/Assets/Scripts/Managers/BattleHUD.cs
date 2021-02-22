ef579c1a151031605638cba55a0b8e6abb42b5a1
MelihCelik00 <melihsafa.c@gmail.com>
2021-02-22 21:08:01 +0300

    scripts folder

---
+++ Magic&Sucuk/Assets/Scripts/Managers/BattleHUD.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Managers
+{
+    public class BattleHUD : MonoBehaviour
+    {
+        public Text nameText;
+        public Slider hpSlider;
+        
+        public void SetHUD(Unit.Unit unit)
+        {
+            nameText.text = unit.unitName;
+            hpSlider.maxValue = unit.maxHP;
+            hpSlider.value = unit.currentHP;
+        }
+
+        public void SetHP(int hp)
+        {
+            hpSlider.value = hp;
+        }
+    }
+}
---
+++ Magic&Sucuk/Assets/Scripts/Unit/BalancedClass.cs
@@ -0,0 +1,73 @@
+﻿using Managers;
+using UnityEngine;
+
+namespace Unit
+{
+    public class BalancedClass : MonoBehaviour
+    {
+        public Unit unit;
+        public BattleHUD playerHUD;
+        private Skills _skills;
+
+        private BattleSystem _battleSystem;
+        private void Start()
+        {
+            unit = GameObject.FindGameObjectWithTag("havai").GetComponent<Unit>();
+            unit.SetUnit("Hava-i",0,15, 100, 100,3,1,1,2);
+            playerHUD = GameObject.FindGameObjectWithTag("playerHUD").GetComponent<BattleHUD>();
+            playerHUD.SetHUD(unit);
+            _battleSystem = GameObject.FindGameObjectWithTag("battleManager").GetComponent<BattleSystem>();
+        }
+
+        public void FirstSkill()
+        {
+            Guard(unit);
+        }
+
+        public void SecondSkill(Unit targetUnit)
+        {
+            WindStrike(targetUnit, unit.magicDamage);
+        }
+
+        public void ThirdSkill(Unit targetUnit)
+        {
+            StreamStrike(targetUnit, unit.magicDamage);
+        }
+
+        public void FourthSkill()
+        {
+            Expose(unit);
+        }
+
+        public void FifthSkill()
+        {
+            AttackBuff(unit);
+        }
+        
+        public void Guard(Unit unit)
+        {
+            unit.CoefficientBuff();
+        }
+        
+        public void WindStrike(Unit unit, int magicDmg)
+        {
+            Debug.Log("Winde geldi");
+            unit.TakeDamage(magicDmg * unit.windCoefficient);
+        }
+        
+        public void StreamStrike(Unit unit, int magicDmg)
+        {
+            unit.TakeDamage(magicDmg*unit.streamCoefficient);
+        }
+        
+        public void Expose(Unit unit)
+        {
+            // Çarpanların 1ini random 1 arttır
+        }
+        
+        public void AttackBuff(Unit unit)
+        {
+            unit.IncreaseAttackDmg();
+        }
+    }
+}
